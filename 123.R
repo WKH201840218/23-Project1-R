5d99707d1cad76750f00826e9a2dcad6127f78a1
wkh <dnjs0076@naver.com>
2023-04-27 21:02:24 +0900

    second commit

--- 123.R
+++ 123.R
@@ -1,20 +1,22 @@
 head(cars)
 dist <- cars[,2]
 dist
-hist(dist,
-     main='Histogarm for 제동거리',
-     xlab = '제동거리',
-     ylab = '빈도수',
-     border='blue',
-     col='green',
-     las=2,
-     breaks = 5)
+hist(iris$Sepal.Length,
+     main = 'Sepal.Length',
+     col='orange')
      
 barplot(table(mcars$cyl),
         main='mtcars',
         col=c('red','green','blue'))
 
-barplot(table(mcars$gear),
+barplot(table(mtcars$gear),
         main='mtcars',
         col=rainbow(3),
         horiz=TRUE)
+
+pie(table(mtcars$cyl),
+    main='mtcars',
+    col=topo.colors(3),
+    radius = 2)
+
+par(mfrow=c(1,1), mar=c(5,4,4,2)+.1)
--- README.md
+++ README.md
@@ -47,6 +47,27 @@ hist(dist,
      las=2,
      breaks = 5)
 ```
+```R
+hist(iris$Sepal.Length,
+     main = 'Sepal.Length',
+     col='orange')
+     
+barplot(table(mcars$cyl),
+        main='mtcars',
+        col=c('red','green','blue'))
+
+barplot(table(mtcars$gear),
+        main='mtcars',
+        col=rainbow(3),
+        horiz=TRUE)
+
+pie(table(mtcars$cyl),
+    main='mtcars',
+    col=topo.colors(3),
+    radius = 2)
+
+par(mfrow=c(1,1), mar=c(5,4,4,2)+.1)
+```
 
 
 <br>
