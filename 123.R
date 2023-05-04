411ea2ce0e59b51b6dde5ad25eda13cf5d7d6d88
wkh <dnjs0076@naver.com>
2023-05-04 20:56:49 +0900

    0504 third commit

--- 123.R
+++ 123.R
@@ -1,4 +1,11 @@
-vars <- c('mpg','dist','drat','wt')
-target <- mtcars[,vars]
-head(target)
-plot(target,main='Multi plots')
+iris.2 <- iris[,3:4]
+group <- as.numberic(iris$Species)
+color <- c('red','green', 'blue')
+plot(iris.2,
+     main='iris plot',
+     pch=c(group),
+     col=color[group])
+legend(x='bottomright',
+       legend = levels(iris$Species),
+       col = c('red','green', 'blue'),
+       pch=c(1:3))
